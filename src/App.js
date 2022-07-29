34ee8c7adbeed8fc393e04896ecb8ec52056214f
Tunahan Gediz <tgediz19@posta.pau.edu.tr>
2022-07-30 01:01:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import AdviceCard from "./components/AdviceCard";
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <AdviceCard></AdviceCard>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
